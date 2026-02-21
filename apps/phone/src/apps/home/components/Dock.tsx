74891f06d1247432934e794828316498a42cce4d
Murai Dev <89431075+mur4i@users.noreply.github.com>
2026-02-21 15:48:22 -0300

    feat: backdrop hack

--- apps/phone/src/apps/home/components/Dock.tsx
+++ apps/phone/src/apps/home/components/Dock.tsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 import { AppIcon } from '@ui/components';
+import React, { useEffect, useState } from 'react';
+import { usePhoneVisibility } from '@os/phone/hooks/usePhoneVisibility';
 
 interface DockProps {
     apps: any[];
@@ -10,7 +12,7 @@ export const Dock: React.FC<DockProps> = ({ apps }) => {
 
     return (
         <div style={{ position: 'absolute', bottom: '24px', left: '16px', right: '16px' }}>
-            <div className="h-[88px] w-full rounded-[32px] backdrop-blur-3xl backdrop-saturate-[200%] bg-white/30 dark:bg-black/30 border border-white/10 dark:border-white/5 flex items-center justify-evenly px-2">
+            <div className="h-[88px] w-full rounded-[32px] backdrop-blur-2xl backdrop-saturate-[200%] bg-white/30 dark:bg-black/30 border border-white/10 dark:border-white/5 flex items-center justify-evenly px-2">
                 {apps.map((app) => (
                     <Link key={app.id} to={app.path} style={{ textDecoration: 'none' }}>
                         <AppIcon {...app} isDockItem={true} />
